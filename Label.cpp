32ca5abc65f65c15361da5a6567877eadc92ee2f
LindaJohansson <linda.singh.johansson@gmail.com>
2016-03-17 14:32:40 +0100

    Added files via upload

--- Label.cpp
+++ Label.cpp
@@ -6,7 +6,31 @@ Label::Label()
 {
 }
 
+Label::Label(int x, int y, int w, int h, int z, std::string string, Color color)
+{
+
+}
 
 Label::~Label()
 {
+}
+
+void Label::SetColor(Color color)
+{
+	glColor3f(color.r, color.g, color.b);
+}
+
+Color Label::GetColor()
+{
+	return this->color;
+}
+
+void Label::SetString(std::string S)
+{
+	string = S;
+}
+
+string Label::GetString()
+{
+	return string;
 }
--- Label.h
+++ Label.h
@@ -1,10 +1,23 @@
 #pragma once
 #include "ControlBaseExtended.h"
+#include "ColorStruct.h"
+
+
 class Label :
 	public ControlBaseExtended
 {
 public:
 	Label();
+	Label(int x, int y, int w, int h, int z, string string, Color color);
 	~Label();
+
+	void SetColor(Color color);
+	Color GetColor();
+	void SetString(string string);
+	string GetString();
+
+private:
+	string string;
+	Color color;
 };
 
