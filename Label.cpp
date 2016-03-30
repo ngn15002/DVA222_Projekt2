029f60abf89376100565e70a5c00077fcb793589
LindaJohansson <linda.singh.johansson@gmail.com>
2016-03-30 13:47:01 +0200

    Added files via upload

--- Label.cpp
+++ Label.cpp
@@ -6,7 +6,42 @@ Label::Label()
 {
 }
 
+Label::Label(int x, int y, int w, int h, int z, std::string S, Color C)
+{
+	posX = x;
+	posY = y;
+	width = w;
+	height = h;
+	zOrdering = z;
+	string = S;
+	color = C;
+}
 
 Label::~Label()
 {
+}
+
+void Label::SetColour(Color color)
+{
+	glColor3f(color.r/255.0, color.g/2550.0, color.b/255.0);
+}
+
+Color Label::GetColour()
+{
+	return this->color;
+}
+
+void Label::SetString(std::string S)
+{
+	string = S;
+}
+
+string Label::GetString()
+{
+	return string;
+}
+
+void Label::OnPaint()
+{
+	DrawString(string, posX, posY);
 }
--- Label.h
+++ Label.h
@@ -1,10 +1,31 @@
 #pragma once
 #include "ControlBaseExtended.h"
+#include "ColorStruct.h"
+#include "MyButton.h"
+#include "Graphix.h"
+
+
 class Label :
 	public ControlBaseExtended
 {
 public:
 	Label();
+	Label(int x, int y, int w, int h, int z, string string, Color color);
 	~Label();
+
+	void SetColour(Color color);
+	Color GetColour();
+	void SetString(string string);
+	string GetString();
+	virtual void OnPaint();
+
+private:
+	string string;
+	Color color;
+	int posX;
+	int posY;
+	int width;
+	int height;
+	int zOrdering;
 };
 
