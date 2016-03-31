b3386642b348c7af94ae168e52c6796b90a2f9af
ngn15002 <diet.theory@gmail.com>
2016-03-31 23:40:15 +0200

    Added files via upload

--- ControlBaseExtended.cpp
+++ ControlBaseExtended.cpp
@@ -2,11 +2,26 @@
 #include "ControlBaseExtended.h"
 
 
-ControlBaseExtended::ControlBaseExtended()
-{
+ControlBaseExtended::ControlBaseExtended() {
 }
 
+ControlBaseExtended::ControlBaseExtended(int x, int y, int w, int h, int z)
+: ControlBase(x, y, w, h) {
+	Z = z;
+}
 
-ControlBaseExtended::~ControlBaseExtended()
-{
+ControlBaseExtended::~ControlBaseExtended() {
+}
+
+void ControlBaseExtended::SetX(int x) {
+	this->X = x;
+}
+void ControlBaseExtended::SetY(int y) {
+	this->Y = y;
+}
+int ControlBaseExtended::GetY() {
+	return this->Y;
+}
+int ControlBaseExtended::GetX() {
+	return this->X;
 }
--- ControlBaseExtended.h
+++ ControlBaseExtended.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "ControlBase.h"
-class ControlBaseExtended :
-	public ControlBase
-{
+class ControlBaseExtended : public ControlBase {
 public:
 	ControlBaseExtended();
+	ControlBaseExtended(int x, int y, int w, int h, int z);
 	~ControlBaseExtended();
+	void SetX(int x);
+	void SetY(int y);
+	int GetY();
+	int GetX();
+
+	int Z;
 };
 
