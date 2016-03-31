a3e4c2cea167aa16f1de06643f76ac57480f920b
ngn15002 <diet.theory@gmail.com>
2016-03-31 13:56:32 +0200

    Added files via upload

--- Window.cpp
+++ Window.cpp
@@ -2,7 +2,7 @@
 #include "Window.h"
 
 
-Window::Window() {
+Window::Window(){
 }
 
 Window::Window(int x, int y, int w, int h, int z) :Container(x,y,w,h,z){
@@ -11,20 +11,47 @@ Window::Window(int x, int y, int w, int h, int z) :Container(x,y,w,h,z){
 	banner = _rectangle(x, y, w, 20);
 }
 
-Window::~Window() {
+Window::~Window(){
+	delete panel;
+	delete frame;
 }
 
-void Window::AddControl(ControlBaseExtended* newControl) {	
+void Window::AddControl(ControlBaseExtended* newControl){	
 	controls.push_back(newControl);
 }
 
-void Window::OnPaint() {
+void Window::OnPaint(){
 	panel->OnPaint();
 	SetColor(0, 0, 255);
 	FillRectangle(banner.X, banner.Y, banner.Width, banner.Height);
 	frame->OnPaint();
 
-	for (int i = 0; i < controls.size(); i++) {
+	for (int i = 0; i < controls.size(); i++){
 		controls[i]->OnPaint();
 	}
+}
+
+void Window::OnMouseDown(int button, int x, int y){
+	if (hit && button == MOUSE_LEFT) {
+		pressed = true;
+	}
+}
+
+void Window::OnMouseUp(int button, int x, int y){
+	pressed = false;
+}
+
+void Window::OnMouseMove(int button, int x, int y){
+	if (x > X && x < X + Width && y>Y && y < Y + banner.Height){
+		hit = true;
+		if(pressed == true){
+			this->X = x;
+			this->Y = y;
+		}
+	} else{
+		pressed = hit = false;
+	}
+}
+
+void Window::OnResize(int width, int height){
 }
--- Window.h
+++ Window.h
@@ -3,13 +3,20 @@
 #include "Frame.h"
 class Window : public Container {
 public:
-	Window();
-	Window(int x, int y, int w, int h, int z);
-	~Window();
-	virtual void OnPaint();
-	virtual void AddControl(ControlBaseExtended* newControl);
 	Panel* panel;
 	Frame* frame;
 	Rect banner;
+	bool hit;
+	bool pressed;
+	
+	Window();
+	Window(int x, int y, int w, int h, int z);
+	~Window();
+	virtual void AddControl(ControlBaseExtended* newControl);
+	virtual void OnPaint();
+	virtual void OnMouseDown(int button, int x, int y);
+	virtual void OnMouseUp(int button, int x, int y);
+	virtual void OnMouseMove(int button, int x, int y);
+	virtual void OnResize(int width, int height);
 };
 
